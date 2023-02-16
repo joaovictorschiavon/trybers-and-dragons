95263741e2be5de514dfc587c262eb14e83efa2c
João Victor Schiavon <schiavonj@yahoo.com.br>
2023-02-16 18:47:08 -0300

    feat: requisite 07

--- src/Character.ts
+++ src/Character.ts
@@ -5,8 +5,8 @@ import Race, { Elf } from './Races';
 import getRandomInt from './utils';
 
 class Character implements Fighter {
-  private race: Race;
-  private archetype: Archetype;
+  private _race: Race;
+  private _archetype: Archetype;
   private _maxLifePoints: number;
   private _lifePoints: number;
   private _strength: number;
@@ -17,23 +17,25 @@ class Character implements Fighter {
   constructor(name: string) {
     this.name = name;
     const dex = getRandomInt(1, 10);
-    this.race = new Elf(name, dex);
+    this._race = new Elf(name, dex);
     this._dexterity = this.race.dexterity;
-    this.archetype = new Mage();
+    this._archetype = new Mage(name);
     this._maxLifePoints = this.race.maxLifePoints / 2;
     this._lifePoints = this._maxLifePoints;
     this._strength = getRandomInt(1, 10);
     this._defense = getRandomInt(1, 10);
     this._energy = { 
-      type_: this.archetype.energyType, 
+      type_: this._archetype.energyType, 
       amount: getRandomInt(1, 10),
     };
   }
 
+  get race(): Race { return this._race; }
+  get archetype(): Archetype { return this._archetype; }
   get lifePoints(): number { return this._lifePoints; }
   get strength(): number { return this._strength; }
   get defense(): number { return this._defense; }
-  get energy(): Energy { return this._energy; }
+  get energy(): Energy { return { ...this._energy }; }
   get dexterity(): number { return this._dexterity; }
 
   receiveDamage(attackPoints: number): number {
